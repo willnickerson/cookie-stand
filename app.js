a2c12f61c8515171b9d0fc06870db1b485aa079e
willnickerson <wnickers@willamette.edu>
2016-08-24 13:25:46 -0700

    code cleanned and made DRYer

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var storeArray = [];
 //This is the object constructor for a store. The salesArray is instaciated as an empty array but will be filled with the hourly and total sales.
 function Store(name, min, max, avg) {
   this.name = name;
@@ -8,7 +9,17 @@ function Store(name, min, max, avg) {
   this.avgSale = avg;
   this.total = 0;
   this.salesArray = [];
+  storeArray.push(this);
 }
+
+//insatanctiating stores
+var pike = new Store('Pike Place', 23, 65, 6.3);
+var seaTac = new Store('SeaTac', 3, 24, 1.2);
+var center = new Store('Seattle Center', 11, 38, 3.7);
+var capHill = new Store('Capitol Hill', 20, 38, 2.3);
+var alki = new Store('Alki', 2, 16, 4.6);
+
+
 
 //calculates the # of customers at a given hour.
 Store.prototype.generateRandom = function() {
@@ -46,7 +57,7 @@ Store.prototype.renderToTable = function() {
       tr.appendChild(td);
     }
   }
-  tableBody.appendChild(tr);
+  return tr;
 };
 
 //This is a helper function that loads the hours of the day into the table head.
@@ -69,74 +80,69 @@ function tableHeadHours() {
 }
 
 //creating table and table head elts
-var salesTable = document.createElement('table');
-var headerContent = tableHeadHours();
-var salesHead = document.createElement('thead');
-var headerRow = document.createElement('tr');
+function createTable() {
+  var salesTable = document.createElement('table');
+  var headerContent = tableHeadHours();
+  var salesHead = document.createElement('thead');
+  var headerRow = document.createElement('tr');
 
-//creating a appending table head content
-for(var i = 0; i < headerContent.length; i++){
-  var th = document.createElement('th');
-  th.textContent = headerContent[i];
-  headerRow.appendChild(th);
-}
-
-//appending table head to table
-salesHead.appendChild(headerRow);
-salesTable.appendChild(salesHead);
-
-//creating tabe body and rendering sales to table.
-var tableBody = document.createElement('tbody');
-salesTable.appendChild(tableBody);
-
-var pike = new Store('Pike Place', 23, 65, 6.3);
-pike.renderToTable();
-
-var seaTac = new Store('SeaTac', 3, 24, 1.2);
-seaTac.renderToTable();
-
-var center = new Store('Seattle Center', 11, 38, 3.7);
-center.renderToTable();
-
-var capHill = new Store('Capitol Hill', 20, 38, 2.3);
-capHill.renderToTable();
-
-var alki = new Store('Alki', 2, 16, 4.6);
-alki.renderToTable();
-
-var tableFoot = document.createElement('tfoot');
-var footRow = document.createElement('tr');
-
-function totalByHour() {
-  var totalArray = ['Totals by hour'];
-  for(i = 0; i < pike.salesArray.length; i++) {
-    var hourTotal = 0;
-    hourTotal += pike.salesArray[i];
-    hourTotal += seaTac.salesArray[i];
-    hourTotal += center.salesArray[i];
-    hourTotal += capHill.salesArray[i];
-    hourTotal += alki.salesArray[i];
-    totalArray[(i + 1)] = hourTotal;
+  //creating a appending table head content
+  for(var i = 0; i < headerContent.length; i++){
+    var th = document.createElement('th');
+    th.textContent = headerContent[i];
+    headerRow.appendChild(th);
   }
-  for(var i = 0; i < headerContent.length; i++) {
-    if(i === 0){
-      var th = document.createElement('th');
-      th.textContent = totalArray[i];
-      footRow.appendChild(th);
-    } else {
-      var td = document.createElement('td');
-      td.textContent = totalArray[i];
-      footRow.appendChild(td);
+
+  //appending table head to table
+  salesHead.appendChild(headerRow);
+  salesTable.appendChild(salesHead);
+
+  //creating table body and appending store rows to table.
+  var tableBody = document.createElement('tbody');
+
+  for(i = 0; i < storeArray.length; i++) {
+    var tr = storeArray[i].renderToTable();
+    tableBody.appendChild(tr);
+  }
+  //appending body to table
+  salesTable.appendChild(tableBody);
+
+  //this function totals sales from all stores to each hour table footer
+  function totalByHour() {
+    //creating footer elts
+    var tableFoot = document.createElement('tfoot');
+    var footRow = document.createElement('tr');
+
+    //looping through sales at each hour and storing the totals in an array
+    var totalArray = ['Totals by hour'];
+    for(i = 0; i < 15; i++) {
+      var hourTotal = 0;
+      for(var j = 0; j < storeArray.length; j++){
+        hourTotal += storeArray[j].salesArray[i];
+        totalArray[(i + 1)] = hourTotal;
+      }
     }
+    //creating table row for the footer and filling it with hour totals
+    for(var i = 0; i < headerContent.length; i++) {
+      if(i === 0){
+        var th = document.createElement('th');
+        th.textContent = totalArray[i];
+        footRow.appendChild(th);
+      } else {
+        var td = document.createElement('td');
+        td.textContent = totalArray[i];
+        footRow.appendChild(td);
+      }
+    }
+    //appending table footer to table.
+    tableFoot.appendChild(footRow);
+    salesTable.appendChild(tableFoot);
   }
-  tableFoot.appendChild(footRow);
-  salesTable.appendChild(tableFoot);
+
+  totalByHour();
+  //appending table to page
+  var main = document.getElementById('store_info');
+  main.appendChild(salesTable);
 }
 
-totalByHour();
-
-
-
-//appending table to main.
-var main = document.getElementById('store_info');
-main.appendChild(salesTable);
+createTable();
